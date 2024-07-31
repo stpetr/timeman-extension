da8b9facec111d32406618d405f22bc05736c70e
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2024-08-01 00:19:15 +0700

    Refactor main store, add categories

--- src/store/main.ts
+++ src/store/main.ts
@@ -2,6 +2,7 @@ import { create, StateCreator } from 'zustand'
 import dayjs from 'dayjs'
 
 import {
+  STORAGE_CATEGORIES,
   STORAGE_ACTIVITIES,
   STORAGE_ACTIVITIES_LOG,
   STORAGE_ACTIVITY_START_TIME,
@@ -9,15 +10,17 @@ import {
 } from 'config/constants'
 import { getStorageItem, setStorageItem } from 'helpers/storage'
 
-import { Activity, ActivityLog, ActivityLogRecord } from 'types/index'
+import { Activity, ActivityLog, ActivityLogRecord, Category } from 'types/index'
 
 type MainStore = {
   activities: Activity[]
+  categories: Category[]
   todayLog: ActivityLogRecord[]
-  currentActivityId: string
+  currentActivityId: string | null
   currentActivityStartedAt: number
   setCurrentActivityId: (id: string | null) => void
   setCurrentActivityStartedAt: (time: number) => void
+  createCategory: (category: Category) => void
   createActivity: (activity: Activity) => void
   moveActivityToTop: (activityId: string) => void
   logActivity: (activity: ActivityLogRecord) => void
@@ -29,10 +32,12 @@ const createMainStore: StateCreator<MainStore> = (set, get) => {
     for (const [key, { oldValue, newValue }] of Object.entries(changes)) {
       if (key === STORAGE_ACTIVITIES) {
         set({ activities: newValue })
+      } else if (key === STORAGE_CATEGORIES) {
+        set({ categories: newValue })
       } else if (key === STORAGE_ACTIVITY_START_TIME) {
         set({ currentActivityStartedAt: newValue || 0 })
       } else if (key === STORAGE_CURRENT_ACTIVITY_ID) {
-        set({ currentActivityId: newValue || '' })
+        set({ currentActivityId: newValue || null })
       } else if (key === STORAGE_ACTIVITIES_LOG) {
         const todayLog = Array.isArray(newValue[todayDate]) ? newValue[todayDate] : []
         set({ todayLog })
@@ -46,23 +51,25 @@ const createMainStore: StateCreator<MainStore> = (set, get) => {
     }
   })
 
-  getStorageItem<Activity[]>(STORAGE_ACTIVITIES).then((activities = []) => {
+  getStorageItem<Category[]>(STORAGE_CATEGORIES, []).then((categories) => {
+    set({ categories })
+  })
+
+  getStorageItem<Activity[]>(STORAGE_ACTIVITIES, []).then((activities = []) => {
     set({ activities })
   })
 
-  getStorageItem<number>(STORAGE_ACTIVITY_START_TIME).then((currentActivityStartedAt) => {
+  getStorageItem<number>(STORAGE_ACTIVITY_START_TIME, 0).then((currentActivityStartedAt) => {
     if (currentActivityStartedAt) {
       set({ currentActivityStartedAt })
     }
   })
 
-  getStorageItem<string>(STORAGE_CURRENT_ACTIVITY_ID).then((currentActivityId) => {
-    if (currentActivityId) {
-      set({ currentActivityId })
-    }
+  getStorageItem<string | null>(STORAGE_CURRENT_ACTIVITY_ID, null).then((currentActivityId) => {
+    set({ currentActivityId })
   })
 
-  getStorageItem<ActivityLog>(STORAGE_ACTIVITIES_LOG).then((activitiesLog) => {
+  getStorageItem<ActivityLog>(STORAGE_ACTIVITIES_LOG, {}).then((activitiesLog) => {
     const todayDate = dayjs().format('YYYY-MM-DD')
     if (Array.isArray(activitiesLog?.[todayDate])) {
       set({ todayLog: activitiesLog[todayDate] })
@@ -70,25 +77,29 @@ const createMainStore: StateCreator<MainStore> = (set, get) => {
   })
 
   return {
+    categories: [],
     activities: [],
     todayLog: [],
     currentActivityStartedAt: 0,
-    currentActivityId: '',
+    currentActivityId: null,
     setCurrentActivityId: async (id) => {
-      setStorageItem(STORAGE_CURRENT_ACTIVITY_ID, id)
+      await setStorageItem(STORAGE_CURRENT_ACTIVITY_ID, id)
     },
-    setCurrentActivityStartedAt: (time) => {
-      setStorageItem(STORAGE_ACTIVITY_START_TIME, time)
+    setCurrentActivityStartedAt: async (time) => {
+      await setStorageItem(STORAGE_ACTIVITY_START_TIME, time)
     },
-    createActivity: (newActivity) => {
-      setStorageItem(STORAGE_ACTIVITIES, [newActivity, ...get().activities])
+    createCategory: async (newCategory) => {
+      await setStorageItem(STORAGE_CATEGORIES, [...get().categories, newCategory])
     },
-    moveActivityToTop: (activityId) => {
+    createActivity: async (newActivity) => {
+      await setStorageItem(STORAGE_ACTIVITIES, [newActivity, ...get().activities])
+    },
+    moveActivityToTop: async (activityId) => {
       const activities = [...get().activities]
       const activityIndex = activities.findIndex(({ id }) => id === activityId)
       // Move activity only if it's not on top
       if (activityIndex > 0) {
-        setStorageItem(STORAGE_ACTIVITIES, [
+        await setStorageItem(STORAGE_ACTIVITIES, [
           activities[activityIndex],
           ...activities.slice(0, activityIndex),
           ...activities.slice(activityIndex + 1),
@@ -97,15 +108,12 @@ const createMainStore: StateCreator<MainStore> = (set, get) => {
     },
     logActivity: async (activityLogRecord) => {
       const todayDate = dayjs().format('YYYY-MM-DD')
-      let activitiesLog = await getStorageItem<ActivityLog>(STORAGE_ACTIVITIES_LOG)
-      if (!activitiesLog) {
-        activitiesLog = {}
-      }
+      const activitiesLog = await getStorageItem<ActivityLog>(STORAGE_ACTIVITIES_LOG, {})
       if (!Array.isArray(activitiesLog?.[todayDate])) {
         activitiesLog[todayDate] = []
       }
       activitiesLog[todayDate] = [...activitiesLog[todayDate], activityLogRecord]
-      setStorageItem(STORAGE_ACTIVITIES_LOG, activitiesLog)
+      await setStorageItem(STORAGE_ACTIVITIES_LOG, activitiesLog)
     },
   }
 }
