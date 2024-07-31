6609b0196d4fe8ba3453dce7ef137ef763052297
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2024-08-01 00:16:28 +0700

    Add types in storage helper. Add default value for getStorageItem

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,21 +1,14 @@
-export const getStorageItem = async <T>(key: string): Promise<T> => {
+export const getStorageItem = async <T>(key: string, defaultValue?: T): Promise<T> => {
   const result = await chrome.storage.local.get(key)
+  if (typeof result[key] === 'undefined' && typeof defaultValue !== 'undefined') {
+    return defaultValue
+  }
+
   return result[key]
 }
 
-export const setStorageItem = (key: string, value: any) => {
-  chrome.storage.local.set({
-    [key]: value
-  })
-}
-
-export const getSyncStorageItem = async (key: string) => {
-  const result = await chrome.storage.sync.get(key)
-  return result[key]
-}
-
-export const setSyncStorageItem = (key: string, value: any) => {
-  chrome.storage.sync.set({
+export const setStorageItem = async <T>(key: string, value: T) => {
+  await chrome.storage.local.set({
     [key]: value
   })
 }
